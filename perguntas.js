9c5f2883b621ef7461fdd9d20728cc8d61a7699b
Cahster <00001094478052SP@aluno.educacao.sp.gov.br>
2024-11-19 11:06:45 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'Sabe como o elétron atende o telefone?',
+    'Próton?'
+)
+
+criaCartao(
+    'Química',
+    'Qual é o doce preferido do átomo?',
+    'O pé-de-moléculas!'
+)
+
+criaCartao(
+    'Química',
+    'O que um álcool disse para o outro?',
+    'Etanóis!'
+)
+
+criaCartao(
+    'Química',
+    'Quer ouvir uma piada sobre potássio?',
+    'KKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKKK'
+)
